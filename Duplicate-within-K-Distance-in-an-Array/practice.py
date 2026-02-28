e3c3de4c69260d06707b2f0df313dc00c8cd1c4d
Natnael Abnew <natnaelanbewpro@gmail.com>
2026-02-28 21:45:24 +0300

    solution

--- Duplicate-within-K-Distance-in-an-Array/practice.py
+++ Duplicate-within-K-Distance-in-an-Array/practice.py
@@ -15,6 +15,10 @@ Input: k = 3, arr[] = [1, 2, 3, 4, 5]
 Output: No
 Explanation: There is no duplicate element in arr[].
 '''
+
+# native of the bruteforce approch
+# with O(n)^2 time complexity and O(n) space complexity
+
 def check_duplicate_within_k(arr,k):
     
     fin = len(arr)
@@ -26,5 +30,34 @@ def check_duplicate_within_k(arr,k):
 
 
 arr = [10, 5, 3, 4, 3, 5, 6]
-print("Yes" if check_duplicate_within_k(arr, 3) else "No")          
+print("Yes" if check_duplicate_within_k(arr, 3) else "No")   
 
+# [Expected Approach] - Using HashSet - O(n) Time and O(k) Space
+'''The idea is to use HashSet to store elements of the array arr[] and check if there is any duplicate present within a k distance. Also remove elements that are present at more than k distance from the current element. Following is a detailed algorithm.
+
+Create an empty HashSet. 
+Traverse all elements from left to right. Let the current element be 'arr[i]' 
+If the current element 'arr[i]' is present in a HashSet, then return true. 
+Else add arr[i] to hash and remove arr[i-k] from hash if i >= k'''
+
+def checkDuplicateWithK(arr,k):
+    #    create empty set 
+    hash_set = set()
+    # determine the length of the array
+    n = len(arr)
+
+    # transeverse the input array 
+    for i in range(n):
+    #    if the element is in the hashset found the duplicate
+    #   else the add the element to the set and continue
+        if arr[i] in hash_set:
+           return True
+        hash_set.add(arr[i])
+
+    if (i  >= k):
+           hash_set.remove(arr[i-k])
+           return False
+        
+    # check case   
+arr = [10, 5, 3, 4, 3, 5, 6]
+print("Yes" if(checkDuplicateWithK(arr,3)) else "No")
