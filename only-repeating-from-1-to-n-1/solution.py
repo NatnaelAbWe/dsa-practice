36f5bc3784b0f43135fb2db0e6aae7181eb9f46b
Natnael Abnew <natnaelanbewpro@gmail.com>
2026-03-05 20:28:17 +0300

    solution 1

---
+++ only-repeating-from-1-to-n-1/solution.py
@@ -0,0 +1,29 @@
+'''Given an array arr[] of size n filled with numbers from 1 to n-1 in random order. The array has only one repetitive element. The task is to find the repetitive element.
+
+Examples:
+
+Input: arr[] = [1, 3, 2, 3, 4]
+Output: 3
+Explanation: The number 3 is the only repeating element.
+
+Input: arr[] = [1, 5, 1, 2, 3, 4]
+Output: 1
+Explanation: The number 1 is the only repeating element.
+
+'''
+
+# native approch 
+
+def search_duplicate(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(i+1,n):
+            if arr[i] == arr[j]:
+                return arr[i]
+    return -1
+    
+if __name__ == "__main__":
+    arr = [1, 3, 2, 3, 4]
+    print(search_duplicate(arr))
+
